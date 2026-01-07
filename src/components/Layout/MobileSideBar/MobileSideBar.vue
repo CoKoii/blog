4d4eb6dfc1a6e2cc2eebdf353c7f6821ccbfb37e
CK. <2655257336@qq.com>
2026-01-07 16:52:59 +0800

    feat: 调整MobileSideBar组件的过渡效果时长，以改善动画流畅度

--- src/components/Layout/MobileSideBar/MobileSideBar.vue
+++ src/components/Layout/MobileSideBar/MobileSideBar.vue
@@ -18,7 +18,7 @@ defineOptions({
 @use './style.scss';
 .fade-enter-active,
 .fade-leave-active {
-  transition: transform 0.1s linear 100ms;
+  transition: transform 0.2s 100ms;
 }
 .fade-enter-from,
 .fade-leave-to {
