8ae077cb28c46efeac13a3ba245bec9550ccdcb0
CK. <2655257336@qq.com>
2026-01-26 13:41:42 +0800

    feat: 添加对 Vue 组件的类型声明

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,12 @@
 /// <reference types="vite/client" />
 /// <reference types="unplugin-vue-markdown/client" />
 
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent
+  export default component
+}
+
 declare module 'virtual:posts-meta' {
   import type { PostMeta } from '@/types/post'
   export const postsMeta: PostMeta[]
